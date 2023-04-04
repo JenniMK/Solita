73a32215a7c2e3d127c23ba9c9b294a006d3c561
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-04 09:33:59 +0300

    add pagination to stations controller file

--- backend/controllers/journeys.js
+++ backend/controllers/journeys.js
@@ -2,14 +2,11 @@ const journeysRouter = require("express").Router();
 const Journey = require("../models/journey");
 
 journeysRouter.get("/", (request, response) => {
-  // Get the 'page' and 'limit' query parameters from the request
   const page = parseInt(request.query.page) || 1;
   const limit = parseInt(request.query.limit) || 20;
 
-  // Calculate the number of items to skip based on the current page and limit
   const skip = (page - 1) * limit;
 
-  // Fetch the paginated data from the database
   Journey.find({})
     .skip(skip)
     .limit(limit)
--- backend/controllers/stations.js
+++ backend/controllers/stations.js
@@ -1,11 +1,19 @@
 const stationsRouter = require("express").Router()
 const Station = require("../models/station")
 
-stationsRouter.get('/', (request, response) => {
-    Station.find({}).then(stations => {
-      response.json(stations)
-    })
-  })
+stationsRouter.get("/", (request, response) => {
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 15;
+  
+    const skip = (page - 1) * limit;
+  
+    Station.find({})
+      .skip(skip)
+      .limit(limit)
+      .then(stations => {
+        response.json(stations);
+      });
+  });
 
 stationsRouter.get("/:id", (request, response, next) => {
     Station.findById(request.params.id)
