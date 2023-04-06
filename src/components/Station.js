a72302eba3b4568fc6aeb398f56e26c3045c75f7
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-06 10:03:56 +0300

    modify Station component to display name and address

--- src/components/Station.js
+++ src/components/Station.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from "react";
-import stationService from "../services/station";
+import React from "react";
 
-const SingleStation = ({ stationId }) => {
-  const [station, setStation] = useState(null);
-
-  useEffect(() => {
-    const chooseStation = async () => {
-      try {
-        const showStation = await stationService.getSingle(stationId);
-        setStation(showStation);
-      } catch (error) {
-        console.error("Error fetching station:", error);
-      }
-    };
-
-    chooseStation();
-  }, [stationId]);
-
+const SingleStation = ({ station }) => {
   if (!station) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h1>{station.name}</h1>
-      <p>{station.address}</p>
+      <h1>{station.Nimi}</h1>
+      <p>{station.Osoite}</p>
       <p>Total journeys starting from the station: {station.journeysStart}</p>
       <p>Total journeys ending at the station: {station.journeysEnd}</p>
     </div>
