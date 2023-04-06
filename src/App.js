004a3893585dfb8d505933f07a4a2d0c1d4a1e22
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-06 10:05:51 +0300

    add useEffect and modify how to return Station component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import "./styles/App.css"
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
+import stationService from './services/station';
+import SingleStation from './components/Station';
 
 const App = () => {
-  const [journey, setJourney] = useState("");
+  const [stations, setStations] = useState([]);
+  const [selectedStationId, setSelectedStationId] = useState(null);
 
-  const showJourney = () => {};
+  useEffect(() => {
+    const fetchStations = async () => {
+      try {
+        const data = await stationService.getAll();
+        setStations(data);
+      } catch (error) {
+        console.error('Error fetching stations:', error);
+      }
+    };
+
+    fetchStations();
+  }, []);
+
+  const selectedStation = selectedStationId ? stations.find(station => station.ID === selectedStationId) : null;
 
   return (
     <div className="App">
       <input
-          className="search-box"
-          type="text"
-          placeholder="Search for the station"
-        />
+        className="search-box"
+        type="text"
+        placeholder="Search for the station"
+      />
       <div className="container">
         <h1>City Bike App</h1>
         <p>
           <button>Show all journeys</button>
         </p>
+        <h2>Show stations</h2>
+        <ul>
+          {stations.map((station) => (
+            <li
+              key={station.ID}
+              onClick={() => setSelectedStationId(station.ID)}
+            >
+              {station.Nimi} {station.Osoite} 
+            </li>
+          ))}
+        </ul>
+        <SingleStation station={selectedStation} />
       </div>
     </div>
   );
 };
 
 export default App;
+
