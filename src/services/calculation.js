b4ca7de86fd2f113122b100989db7e54fe793e6b
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-07 20:32:01 +0300

    fix service file for calculations

---
+++ src/services/calculation.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/calculations";
+
+const getAll = async () => {
+    const request = await axios.get(baseUrl)
+    return request.data
+  }
+
+export default { getAll };
+
