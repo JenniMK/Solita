09d2e374f32a3ee2bb8391468e070d3155674c77
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-07 20:31:04 +0300

    fix calculations of total journeys to be displayed correctly

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
 import React, { useState, useEffect } from "react";
 import "./styles/App.css";
 import stationService from "./services/station";
+import calculationService from "./services/calculation";
 import Station from "./components/Station";
 
 const App = () => {
   const [stations, setStations] = useState([]);
-  const [selectedStation, setSelectedStation] = useState(null);
-
-  const fetchStations = async (page) => {
-    try {
-      const data = await stationService.getAll(page);
-      setStations((prevStations) => [...prevStations, ...data]);
-    } catch (error) {
-      console.error("Error fetching stations:", error);
-    }
-  };
+  const [calcs, setCalcs] = useState([]);
 
   useEffect(() => {
-    fetchStations(1);
+    const fetchStations = async () => {
+      try {
+        const data = await stationService.getAll();
+        setStations(data);
+      } catch (error) {
+        console.error("Error fetching stations:", error);
+      }
+    };
+
+    fetchStations();
   }, []);
 
-  const handleLoadMore = () => {
-    const nextPage = Math.floor(stations.length / 15) + 1;
-    fetchStations(nextPage);
-  };
-
-  const handleStationClick = async (id) => {
-    try {
-      const stationData = await stationService.getSingle(id);
-      setSelectedStation(stationData);
-    } catch (error) {
-      console.error("Error fetching station data:", error);
-    }
-  };
+  useEffect(() => {
+    calculationService.getAll().then((calcs) => {
+      console.log(calcs);
+      setCalcs(calcs);
+    });
+  }, []);
 
   return (
     <div className="App">
       <div className="container">
         <h1>City Bike App</h1>
-
         <h2>Show stations</h2>
-        <ul>
-        {
-  stations.map((station) => (
-    <li key={station._id} onClick={() => handleStationClick(station.ID)}>
-      {station.Nimi}
-    </li>
-  ))}
-
-
-        </ul>
-
-        <button onClick={handleLoadMore}>Load more stations</button>
-        {selectedStation && (
-          <Station station={selectedStation} />
-        )}
+        <div>
+          {stations.map((station) => (
+            <Station key={station.ID} station={station} calcs={calcs} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default App;
+
 
--- src/components/Station.js
+++ src/components/Station.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-const Station = ({ station }) => {
-  console.log(station);
+const Station = ({ station, calcs }) => {
   if (!station) {
     return <div>Loading...</div>;
   }
 
+  const calcData = calcs.find((calc) => calc.ID === station.ID) || {
+    journeyStart: 0,
+    journeyEnd: 0,
+  };
+
   return (
     <div>
-      <h1>{station.Nimi}</h1>
+      <h3>{station.Nimi}</h3>
       <p>{station.Osoite}</p>
-      <p>Total journeys starting from the station: {station.journeyStart}</p>
-      <p>Total journeys ending at the station: {station.journeyEnd}</p>
+      <p>Total journeys starting from the station: {calcData.journeyStart}</p>
+      <p>Total journeys ending at the station: {calcData.journeyEnd}</p>
     </div>
   );
 };
