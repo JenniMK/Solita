f9df9502598da435dcf31a1068904a8c80f76437
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-12 12:11:32 +0300

    replace useEffect with useQuery for faster performance

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 import "./styles/App.css";
 import Station from './components/Station';
 import Pagination from './components/Pagination';
@@ -6,30 +7,20 @@ import stationsService from './services/station';
 import calculationsService from './services/calculation';
 
 function App() {
-  const [stations, setStations] = useState([]);
-  const [calcs, setCalcs] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [loading, setLoading] = useState(true);
-  
-  useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage]);
+  const limit = 20;
 
-  const fetchData = async (page) => {
-    setLoading(true);
-    const limit = 20;
+  const stationsQuery = useQuery(
+    ['stations', currentPage], 
+    () => stationsService.getPaginated(limit, currentPage),
+    { keepPreviousData: true }
+  );
 
-    const stationsData = await stationsService.getPaginated(limit, page);
-    setStations(stationsData.stations);
-    console.log(stations)
-
-    const calculationsData = await calculationsService.getAll(limit, page);
-    setCalcs(calculationsData.results);
-    setTotalPages(calculationsData.totalPages);
-
-    setLoading(false);
-  };
+  const calculationsQuery = useQuery(
+    ['calculations', currentPage], 
+    () => calculationsService.getAll(limit, currentPage),
+    { keepPreviousData: true }
+  );
 
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -39,22 +30,21 @@ function App() {
     setCurrentPage(currentPage - 1);
   };
 
-  if (loading) return "Loading...";
-
+  if (stationsQuery.isLoading || calculationsQuery.isLoading) return "Loading...";
 
   return (
     <div className="App">
       <div className="container">
         <h1>City Bike App</h1>
         <h2>Show stations</h2>
-        {stations && calcs && stations.map((station) => (
-          <Station key={station.id} station={station} calcs={calcs} />
+        {stationsQuery.data?.stations && calculationsQuery.data?.results && stationsQuery.data.stations.map((station) => (
+          <Station key={station.id} station={station} calcs={calculationsQuery.data.results} />
         ))}
         <Pagination
           handlePrevPage={handlePrevPage}
           handleNextPage={handleNextPage}
           currentPage={currentPage}
-          totalPages={totalPages}
+          totalPages={calculationsQuery.data?.totalPages}
         />
       </div>
     </div>
