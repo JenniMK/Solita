8d77468f34a825c1928882cdaf9149e321827b28
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-14 18:21:25 +0300

    add StationSearch component for search functionality

---
+++ src/components/StationSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StationSearch = ({ station, onClick }) => {
+  return (
+    <div onClick={onClick}>
+      <p>{station.Nimi}</p>
+    </div>
+  );
+};
+
+export default StationSearch;
