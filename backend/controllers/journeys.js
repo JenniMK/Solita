a3808566a8ab164151c3d09a0af1c8d21be52472
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-15 16:21:27 +0300

    add calculations of total pages to controllerfiles

--- backend/controllers/journeys.js
+++ backend/controllers/journeys.js
@@ -1,30 +1,37 @@
 const journeysRouter = require("express").Router();
 const Journey = require("../models/journey");
 
-journeysRouter.get("/", (request, response) => {
-  const page = parseInt(request.query.page) || 1;
-  const limit = parseInt(request.query.limit) || 15;
+journeysRouter.get("/", async (request, response, next) => {
+  try {
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 15;
 
-  const skip = (page - 1) * limit;
+    const skip = (page - 1) * limit;
 
-  Journey.find({})
-    .skip(skip)
-    .limit(limit)
-    .then(journeys => {
-      response.json(journeys);
-    });
+    const journeys = await Journey.find({})
+      .skip(skip)
+      .limit(limit);
+
+    const totalJourneys = await Journey.countDocuments();
+    const totalJourneyPages = Math.ceil(totalJourneys / limit);
+
+    response.json({ journeys, totalJourneyPages });
+  } catch (error) {
+    next(error);
+  }
 });
 
-journeysRouter.get("/:id", (request, response, next) => {
-  Journey.findById(request.params.id)
-    .then(journey => {
-      if (journey) {
-        response.json(journey);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch(error => next(error));
+journeysRouter.get("/:id", async (request, response, next) => {
+  try {
+    const journey = await Journey.findById(request.params.id);
+    if (journey) {
+      response.json(journey);
+    } else {
+      response.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = journeysRouter;
--- backend/controllers/stations.js
+++ backend/controllers/stations.js
@@ -1,19 +1,24 @@
-const stationsRouter = require("express").Router()
-const Station = require("../models/station")
-const Journey = require("../models/journey")
+const stationsRouter = require("express").Router();
+const Station = require("../models/station");
+const Journey = require("../models/journey");
 
-stationsRouter.get("/", (request, response) => {
-  const page = parseInt(request.query.page) || 1;
-  const limit = parseInt(request.query.limit) || 15;
-  const skip = (page - 1) * limit;
+stationsRouter.get("/", async (request, response, next) => {
+  try {
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 15;
+    const skip = (page - 1) * limit;
 
-  Station.find({})
-    .skip(skip)
-    .limit(limit)
-    .then(stations => {
-      response.json(stations);
-    })
-    .catch(error => next(error));
+    const stations = await Station.find({})
+      .skip(skip)
+      .limit(limit);
+
+    const totalStations = await Station.countDocuments();
+    const totalStationPages = Math.ceil(totalStations / limit);
+
+    response.json({ stations, totalStationPages });
+  } catch (error) {
+    next(error);
+  }
 });
 
 stationsRouter.get("/search", async (request, response, next) => {
@@ -31,36 +36,17 @@ stationsRouter.get("/search", async (request, response, next) => {
   }
 });
 
-
-  stationsRouter.get("/:id", (request, response, next) => {
-    Station.findById(request.params.id)
-      .then(station => {
-        if (station) {
-          response.json(station);
-        } else {
-          response.status(404).end();
-        }
-      })
-      .catch(error => next(error));
-  });
-
-stationsRouter.get("/search", async (request, response, next) => {
-  const query = request.query.query;
-
-  if (!query) {
-    return response.status(400).json({ error: "Missing query parameter" });
-  }
-
+stationsRouter.get("/:id", async (request, response, next) => {
   try {
-    const stations = await Station.find({
-      Nimi: { $regex: query, $options: "i" },
-    });
-    response.json(stations);
+    const station = await Station.findById(request.params.id);
+    if (station) {
+      response.json(station);
+    } else {
+      response.status(404).end();
+    }
   } catch (error) {
     next(error);
   }
 });
 
-
-module.exports = stationsRouter
-
+module.exports = stationsRouter;
