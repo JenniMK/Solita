1f8a2c60798bb887f5cb5facd5c64b2f05fadc61
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-25 13:27:20 +0300

    add required fields to journey schema

--- backend/models/journey.js
+++ backend/models/journey.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const journeySchema = new mongoose.Schema({
   Departure: Date,
   Return: Date,
-  "Departure station id": Number,
-  "Departure station name": String,
-  "Return station id": Number,
-  "Return station name": String,
-  "Covered distance (m)": Number,
-  "Duration_sec": Number
+  "Departure station id": { type: Number, required: true },
+  "Departure station name": { type: String, required: true },
+  "Return station id": { type: Number, required: true },
+  "Return station name": { type: String, required: true },
+  "Covered distance (m)": { type: Number, required: true },
+  "Duration_sec": { type: Number, required: true }
 });
 
 journeySchema.index({ "Departure station id": 1, "Departure station name": 1, "Return station id": 1, "Return station name": 1, "Covered distance (m)": 1, "Duration_sec": 1 });
