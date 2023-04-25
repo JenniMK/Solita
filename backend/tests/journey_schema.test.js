d80439c3fe9a73cc96d64a3e3424eb60b2e50b79
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-25 13:29:49 +0300

    add tests for journey schema

---
+++ backend/tests/journey_schema.test.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const Journey = require('../models/journey');
+
+describe('Journey model', () => {
+  let mongoServer;
+
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterEach(async () => {
+    await Journey.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+  test('creating a new journey', async () => {
+    const journeyData = {
+      Departure: new Date(),
+      Return: new Date(),
+      "Departure station id": 1,
+      "Departure station name": "Station A",
+      "Return station id": 2,
+      "Return station name": "Station B",
+      "Covered distance (m)": 1000,
+      "Duration_sec": 600
+    };
+
+    const journey = new Journey(journeyData);
+    const savedJourney = await journey.save();
+
+    expect(savedJourney).toMatchObject(journeyData);
+  }, 10000);
+
+  test('creating a journey with missing required fields should fail', async () => {
+    const journeyData = {
+      Departure: new Date(),
+      Return: new Date(),
+      "Departure station id": 1,
+      "Departure station name": "Station A",
+      "Return station id": 2,
+      "Return station name": "Station B"
+    };
+  
+    const journey = new Journey(journeyData);
+
+    await expect(journey.save()).rejects.toThrowError(mongoose.Error.ValidationError);
+  }, 10000);
+
+  test('transforming journey document toJSON', async () => {
+    const journeyData = {
+      Departure: new Date(),
+      Return: new Date(),
+      "Departure station id": 1,
+      "Departure station name": "Station A",
+      "Return station id": 2,
+      "Return station name": "Station B",
+      "Covered distance (m)": 1000,
+      "Duration_sec": 600
+    };
+
+    const journey = new Journey(journeyData);
+    const savedJourney = await journey.save();
+
+    const jsonResponse = savedJourney.toJSON();
+
+    expect(jsonResponse).toHaveProperty('id');
+    expect(jsonResponse).not.toHaveProperty('_id');
+    expect(jsonResponse).not.toHaveProperty('__v');
+  }, 10000);
+});
