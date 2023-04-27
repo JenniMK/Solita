0bc2ebc5a35736962c2a91c2759a3253398ca744
Jenni Kuvaja <jenni.kuvaja@gmail.com>
2023-04-27 08:04:39 +0300

    add test for station's pagination

--- src/components/Station.test.js
+++ src/components/Station.test.js
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Station from "./Station";
+import StationPagination from "./StationPagination";
+
 
 describe("Station component", () => {
   test("renders loading message when station prop is not provided", () => {
@@ -32,5 +34,60 @@ describe("Station component", () => {
     expect(screen.queryByText(station.Osoite)).not.toBeInTheDocument();
     expect(screen.queryByText(/Total journeys starting from the station:/i)).not.toBeInTheDocument();
     expect(screen.queryByText(/Total journeys ending at the station:/i)).not.toBeInTheDocument();
+  });
+});
+
+describe("StationPagination component", () => {
+  test("renders the correct current page and total pages", () => {
+    render(
+      <StationPagination
+        currentStationPage={2}
+        totalStationPages={5}
+        handleNextStationPage={() => {}}
+        handlePrevStationPage={() => {}}
+      />
+    );
+
+    expect(screen.getByText("Page 2 of 5")).toBeInTheDocument();
+  });
+
+  test("previous button is disabled on the first page", () => {
+    render(
+      <StationPagination
+        currentStationPage={1}
+        totalStationPages={5}
+        handleNextStationPage={() => {}}
+        handlePrevStationPage={() => {}}
+      />
+    );
+
+    expect(screen.getByRole("button", { name: /previous/i })).toBeDisabled();
+  });
+
+  test("next button is disabled on the last page", () => {
+    render(
+      <StationPagination
+        currentStationPage={5}
+        totalStationPages={5}
+        handleNextStationPage={() => {}}
+        handlePrevStationPage={() => {}}
+      />
+    );
+
+    expect(screen.getByRole("button", { name: /next/i })).toBeDisabled();
+  });
+
+  test("previous and next buttons are enabled on a middle page", () => {
+    render(
+      <StationPagination
+        currentStationPage={3}
+        totalStationPages={5}
+        handleNextStationPage={() => {}}
+        handlePrevStationPage={() => {}}
+      />
+    );
+
+    expect(screen.getByRole("button", { name: /previous/i })).toBeEnabled();
+    expect(screen.getByRole("button", { name: /next/i })).toBeEnabled();
   });
 });
